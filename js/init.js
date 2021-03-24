bbad5d7c87b7b82f2f7cb19b12aeed28d843b74b
varivonne <81310703+varivonne@users.noreply.github.com>
2021-03-24 11:09:14 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyBKXKRaQCWdfiSCZR39wunK0QApZMhgeqE",
-    authDomain: "pauten43.firebaseapp.com",
-    projectId: "pauten43",
-    storageBucket: "pauten43.appspot.com",
-    messagingSenderId: "167024006136",
-    appId: "1:167024006136:web:2766577637b6ab37c43c0d",
-    measurementId: "G-S4KD63PHHW"
+    apiKey: "AIzaSyB6MAP1vM7EQ8yHDvgHveDi78gy0-VwM-A",
+    authDomain: "autentica42k.firebaseapp.com",
+    projectId: "autentica42k",
+    storageBucket: "autentica42k.appspot.com",
+    messagingSenderId: "259134072371",
+    appId: "1:259134072371:web:7e09a4191843b2e46ea546",
+    measurementId: "G-J4RBTHRG1K"
     });
