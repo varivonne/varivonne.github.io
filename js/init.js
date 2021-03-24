30d075a310391fb85ccfd0a9b65a5828a910be59
varivonne <81310703+varivonne@users.noreply.github.com>
2021-03-24 10:42:04 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBKXKRaQCWdfiSCZR39wunK0QApZMhgeqE",
+    authDomain: "pauten43.firebaseapp.com",
+    projectId: "pauten43",
+    storageBucket: "pauten43.appspot.com",
+    messagingSenderId: "167024006136",
+    appId: "1:167024006136:web:2766577637b6ab37c43c0d",
+    measurementId: "G-S4KD63PHHW"
+    });
